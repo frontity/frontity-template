ca30763e0c5fc62b45b77ee32744666abbdec4dc
Luis Herranz <luisherranz@gmail.com>
2019-08-23 11:59:52 +0200

    style(settings): prettify frontity.settings.js

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,51 +1,36 @@
 const settings = {
-  "name": "codesandbox-frontity",
-  "state": {
-    "frontity": {
-      "url": "https://test.frontity.io",
-      "title": "Test Frontity Blog",
-      "description": "WordPress installation for Frontity development"
+  name: "codesandbox-frontity",
+  state: {
+    frontity: {
+      url: "https://test.frontity.io",
+      title: "Test Frontity Blog",
+      description: "WordPress installation for Frontity development"
     }
   },
-  "packages": [
+  packages: [
     {
-      "name": "@frontity/mars-theme",
-      "state": {
-        "theme": {
-          "menu": [
-            [
-              "Home",
-              "/"
-            ],
-            [
-              "Nature",
-              "/category/nature/"
-            ],
-            [
-              "Travel",
-              "/category/travel/"
-            ],
-            [
-              "Japan",
-              "/tag/japan/"
-            ],
-            [
-              "About Us",
-              "/about-us/"
-            ]
+      name: "@frontity/mars-theme",
+      state: {
+        theme: {
+          menu: [
+            ["Home", "/"],
+            ["Nature", "/category/nature/"],
+            ["Travel", "/category/travel/"],
+            ["Japan", "/tag/japan/"],
+            ["About Us", "/about-us/"]
           ],
-          "featured": {
-            "showOnList": false,
-            "showOnPost": false
+          featured: {
+            showOnList: false,
+            showOnPost: false
           }
         }
       }
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "api": "https://test.frontity.io/wp-json"
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          api: "https://test.frontity.io/wp-json"
         }
       }
     },
